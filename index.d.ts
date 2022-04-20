c7a4a05fd2f3d60ef93bd3b4578dd389d6c7a4f8
rhysd <lin90162@yahoo.co.jp>
2022-04-20 20:48:47 +0900

    support React v18

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,7 @@
-/// <reference types="react" />
 import * as React from 'react';
 export declare type BytesizeIconStrokeWidth = 'ultra-light' | 'thin' | 'light' | 'regular' | 'medium' | 'bold' | 'heavy';
 export declare type BytesizeIconSize = 'small' | 'regular' | 'larger' | 'largest';
-export interface BytesizeIconProps extends React.Props<BytesizeIcon> {
+export interface BytesizeIconProps {
     readonly name: BytesizeIconName;
     readonly thickness?: BytesizeIconStrokeWidth;
     readonly size?: BytesizeIconSize;
@@ -10,4 +9,4 @@ export interface BytesizeIconProps extends React.Props<BytesizeIcon> {
 export default class BytesizeIcon extends React.PureComponent<BytesizeIconProps, {}> {
     render(): JSX.Element;
 }
-export declare type BytesizeIconName = 'activity' | 'alert' | 'archive' | 'arrow-bottom' | 'arrow-left' | 'arrow-right' | 'arrow-top' | 'backwards' | 'ban' | 'bell' | 'book' | 'bookmark' | 'calendar' | 'camera' | 'caret-bottom' | 'caret-left' | 'caret-right' | 'caret-top' | 'cart' | 'checkmark' | 'chevron-bottom' | 'chevron-left' | 'chevron-right' | 'chevron-top' | 'clock' | 'close' | 'code' | 'compose' | 'creditcard' | 'download' | 'edit' | 'eject' | 'ellipsis-horizontal' | 'ellipsis-vertical' | 'end' | 'export' | 'external' | 'eye' | 'file' | 'fire' | 'flag' | 'folder-open' | 'folder' | 'forwards' | 'gift' | 'github' | 'heart' | 'home' | 'import' | 'inbox' | 'info' | 'lightning' | 'link' | 'location' | 'lock' | 'mail' | 'menu' | 'msg' | 'minus' | 'music' | 'mute' | 'options' | 'paperclip' | 'pause' | 'photo' | 'play' | 'plus' | 'portfolio' | 'print' | 'reload' | 'reply' | 'search' | 'send' | 'settings' | 'star' | 'start' | 'tag' | 'trash' | 'twitter' | 'unlock' | 'upload' | 'user' | 'video' | 'volume' | 'work';
+export declare type BytesizeIconName = 'activity' | 'alert' | 'archive' | 'arrow-bottom' | 'arrow-left' | 'arrow-right' | 'arrow-top' | 'backwards' | 'bag' | 'ban' | 'bell' | 'book' | 'bookmark' | 'calendar' | 'camera' | 'caret-bottom' | 'caret-left' | 'caret-right' | 'caret-top' | 'cart' | 'checkmark' | 'chevron-bottom' | 'chevron-left' | 'chevron-right' | 'chevron-top' | 'clipboard' | 'clock' | 'close' | 'code' | 'compose' | 'creditcard' | 'desktop' | 'download' | 'edit' | 'eject' | 'ellipsis-horizontal' | 'ellipsis-vertical' | 'end' | 'export' | 'external' | 'eye' | 'feed' | 'file' | 'filter' | 'flag' | 'folder-open' | 'folder' | 'forwards' | 'fullscreen-exit' | 'fullscreen' | 'gift' | 'github' | 'heart' | 'home' | 'import' | 'inbox' | 'info' | 'lightning' | 'link' | 'location' | 'lock' | 'mail' | 'menu' | 'msg' | 'microphone' | 'minus' | 'mobile' | 'moon' | 'move' | 'music' | 'mute' | 'options' | 'paperclip' | 'pause' | 'photo' | 'play' | 'plus' | 'portfolio' | 'print' | 'reload' | 'reply' | 'search' | 'send' | 'settings' | 'sign-in' | 'sign-out' | 'star' | 'start' | 'tag' | 'telephone' | 'trash' | 'twitter' | 'unlock' | 'upload' | 'user' | 'video' | 'volume' | 'work' | 'zoom-in' | 'zoom-out' | 'zoom-reset';
